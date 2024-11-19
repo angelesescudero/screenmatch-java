20b3dddf97765f7c6c252e792d24679c21fa8719
Angeles Escudero Gedge <aescuderogedge@gmail.com>
2024-11-19 09:43:56 -0300

    avances en spring

---
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,43 @@
+package com.aluracursos.screenmatch;
+
+import com.aluracursos.screenmatch.model.DatosEpisodios;
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		String url;
+		var json = consumoApi.obtenerDatos(url="https://www.omdbapi.com/?t=game+of+thrones&apikey=8f014af9");
+		System.out.println(json);
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos (json, DatosSerie.class);
+		System.out.println(datos);
+		json = consumoApi.obtenerDatos(url="https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=8f014af9");
+		DatosEpisodios episodios = conversor.obtenerDatos(json, DatosEpisodios.class);
+		System.out.println(episodios);
+
+		List<DatosTemporadas> temporadas = new ArrayList<>();
+		for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
+			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season="+i+"&apikey=8f014af9");
+			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+			temporadas.add(datosTemporadas);
+		}
+		temporadas.forEach(System.out::println);
+	}
+}
---
+++ src/main/java/com/aluracursos/screenmatch/model/DatosEpisodios.java
@@ -0,0 +1,14 @@
+package com.aluracursos.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public record DatosEpisodios(
+        @JsonAlias("Title") String Titulo,
+        @JsonAlias("Episode") Integer numeroEpisodio,
+        @JsonAlias("imdbRating") String evaluacion,
+        @JsonAlias("Released") String fechaDeLanzamiento
+) {
+}
